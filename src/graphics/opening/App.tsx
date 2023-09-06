62faecf91cfc2a9ab8470b75e3c187f11e2758d6
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-09-06 19:26:41 +0900

    Add vanilla-extract and hot reload

--- src/graphics/opening/App.tsx
+++ src/graphics/opening/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { background } from './App.css';
+import backgroundImg from './assets/background2.png';
 
 export const App = () => {
     return (
-        <div>
+    <>
+        <img className={background} src={backgroundImg} />
+        <div className={background}>
             <h1>Hello, world!</h1>
         </div>
+    </>
     );
 }
